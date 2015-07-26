ee1704e26769afc16ee48746c2be9dd34c5b226c
Manuel <manuel.contreras.morillo@gmail.com>
2015-07-26 20:25:46 +0200

    dialect

--- models/models.js
+++ models/models.js
@@ -18,7 +18,7 @@ var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite
 var sequelize = new Sequelize(DB_name, user, pwd,
-								{dialect: protocol, 
+								{dialect: dialect, 
 								 protocol: protocol,
 								 port: port,
 								 host: host,
